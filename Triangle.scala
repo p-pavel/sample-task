7483d2e48252341d4b1b9b39e021b237ca86a9bb
Pavel Perikov <perikov@gmail.com>
2023-11-17 19:55:22 +0400

    I think I implemented the algo already

--- Triangle.scala
+++ Triangle.scala
@@ -6,11 +6,21 @@ case class Path(weight: Weight, vertices: Seq[Weight])
 
 object Path: 
   /** induction basis*/
-  def empty = Path(0, Seq.empty)
+  val empty = Path(0, Seq.empty)
   def choose(w: Weight, left: Path, right: Path) = 
     val candidate = if left.weight < right.weight then left else right
     Path(w + candidate.weight, w +: candidate.vertices)
-
+  def walkData(data: TriangleData): Seq[Path] = 
+    require(data.length > 0)
+    val reversed = data.reverse
+    def computePaths(previous: Seq[Path], current: Seq[Weight]): Seq[Path] = 
+      require(previous.length == current.length + 1)
+      current.zipWithIndex.map {(weight, index) => 
+        Path.choose(weight, previous(index), previous(index+1))
+      }
+    
+    val phony: Seq[Path] = Seq.fill(reversed.head.length + 1)(empty)
+    reversed.foldLeft(phony)(computePaths)
 @main
 def computePath = 
   println("Hello")
--- Triangle.test.scala
+++ Triangle.test.scala
@@ -5,3 +5,12 @@ class TriangleTest extends munit.FunSuite:
     val res = Path.choose(w, left,right)
     assertEquals(res, Path(w, Seq(w)))
   }
+
+  test("Single row storage") {
+    val data = Seq(Seq(10,20,30))
+    val res = Path.walkData(data)
+    val expected = Seq(Path(10, Seq(10)), Path(20, Seq(20)), Path(30, Seq(30)))
+    assertEquals(res, expected)
+  }
+  
+  
